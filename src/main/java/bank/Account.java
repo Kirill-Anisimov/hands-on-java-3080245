353d37bca32ee3d44ace0a468418933237275ab2
Kirill Anisimov <46004196+Kirill-Anisimov@users.noreply.github.com>
2024-05-31 14:06:38 +0000

    deposit method created with exception

--- src/main/java/bank/Account.java
+++ src/main/java/bank/Account.java
@@ -1,5 +1,7 @@
 package bank;
 
+import bank.exceptions.AmountException;
+
 public class Account {
 
   private int id;
@@ -34,5 +36,18 @@ public class Account {
 
   public void setBalance(double balance) {
     this.balance = balance;
+  }
+
+  public void deposit(double amount) throws AmountException {
+    if (amount < 1) {
+      throw new AmountException("The minimum deposit is 1.00");
+    } else {
+      double newBalance = balance + amount;
+      setBalance(newBalance);
+    }
+  }
+
+  public void withdraw(double amount) {
+    
   }
 }
